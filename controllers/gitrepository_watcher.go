4750231861d02c8b5b8468bc0ad1fb22ec655200
stefanprodan <stefan.prodan@gmail.com>
2020-07-07 22:08:46 +0300

    Download and extract artifact

--- controllers/gitrepository_watcher.go
+++ controllers/gitrepository_watcher.go
@@ -18,6 +18,10 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -27,6 +31,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	sourcev1 "github.com/fluxcd/source-controller/api/v1alpha1"
+	"github.com/stefanprodan/source-watcher/pkg/untar"
 )
 
 // GitRepositoryWatcher watches GitRepository objects for revision changes
@@ -40,9 +45,11 @@ type GitRepositoryWatcher struct {
 // +kubebuilder:rbac:groups=source.fluxcd.io,resources=gitrepositories/status,verbs=get
 
 func (r *GitRepositoryWatcher) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	// set timeout for the reconciliation
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
+	// get source object
 	var repository sourcev1.GitRepository
 	if err := r.Get(ctx, req.NamespacedName, &repository); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
@@ -50,6 +57,32 @@ func (r *GitRepositoryWatcher) Reconcile(req ctrl.Request) (ctrl.Result, error) 
 
 	log := r.Log.WithValues(strings.ToLower(repository.Kind), req.NamespacedName)
 	log.Info("New revision detected", "revision", repository.Status.Artifact.Revision)
+
+	// create tmp dir
+	tmpDir, err := ioutil.TempDir("", repository.Name)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to create temp dir, error: %w", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	// download and extract artifact
+	summary, err := r.fetchArtifact(ctx, repository, tmpDir)
+	if err != nil {
+		log.Error(err, "unable to fetch artifact")
+		return ctrl.Result{}, err
+	}
+	log.Info(summary)
+
+	// list artifact content
+	files, err := ioutil.ReadDir(tmpDir)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("faild to list files, error: %w", err)
+	}
+
+	// do something with the artifact content
+	for _, f := range files {
+		log.Info("Processing " + f.Name())
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -59,4 +92,44 @@ func (r *GitRepositoryWatcher) SetupWithManager(mgr ctrl.Manager) error {
 		For(&sourcev1.GitRepository{}).
 		WithEventFilter(GitRepositoryRevisionChangePredicate{}).
 		Complete(r)
+}
+
+func (r *GitRepositoryWatcher) fetchArtifact(ctx context.Context, repository sourcev1.GitRepository, dir string) (string, error) {
+	if repository.Status.Artifact == nil {
+		return "", fmt.Errorf("respository %s does not containt an artifact", repository.Name)
+	}
+
+	url := repository.Status.Artifact.URL
+
+	// for local run:
+	// kubectl -n gitops-system port-forward svc/source-controller 8080:80
+	// export SOURCE_HOST=localhost:8080
+	if hostname := os.Getenv("SOURCE_HOST"); hostname != "" {
+		url = fmt.Sprintf("http://%s/gitrepository/%s/%s/latest.tar.gz", hostname, repository.Namespace, repository.Name)
+	}
+
+	// download the tarball
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create HTTP request, error: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	if err != nil {
+		return "", fmt.Errorf("failed to download artifact from %s, error: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	// check response
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("faild to download artifact, status: %s", resp.Status)
+	}
+
+	// extract
+	summary, err := untar.Untar(resp.Body, dir)
+	if err != nil {
+		return "", fmt.Errorf("faild to untar artifact, error: %w", err)
+	}
+
+	return summary, nil
 }
