fluxcd/source-controller

commit 8e1b213da5d31527432cced4fe9f93167fe0d535

Change CRD domain to 'source.toolkit.fluxcd.io'
Due to required domain changes for the helm-controller so that it
can co-exist in a cluster with the Helm Operator, other Toolkit
components are moving to a *.toolklit.fluxcd.io domain too.

config/rbac/role.yaml
@@ -7,7 +7,7 @@ metadata:
   name: manager-role
 rules:
 - apiGroups:
-  - source.fluxcd.io
+  - source.toolkit.fluxcd.io
   resources:
   - gitrepositories
   verbs:
@@ -19,7 +19,7 @@ rules:
   - update
   - watch
 - apiGroups:
-  - source.fluxcd.io
+  - source.toolkit.fluxcd.io
   resources:
   - gitrepositories/status
   verbs:
@@ -27,7 +27,7 @@ rules:
   - patch
   - update
 - apiGroups:
-  - source.fluxcd.io
+  - source.toolkit.fluxcd.io
   resources:
   - helmcharts
   verbs:
@@ -39,7 +39,7 @@ rules:
   - update
   - watch
 - apiGroups:
-  - source.fluxcd.io
+  - source.toolkit.fluxcd.io
   resources:
   - helmcharts/finalizers
   verbs:
@@ -47,7 +47,7 @@ rules:
   - patch
   - update
 - apiGroups:
-  - source.fluxcd.io
+  - source.toolkit.fluxcd.io
   resources:
   - helmcharts/status
   verbs:
@@ -55,7 +55,7 @@ rules:
   - patch
   - update
 - apiGroups:
-  - source.fluxcd.io
+  - source.toolkit.fluxcd.io
   resources:
   - helmrepositories
   verbs:
@@ -67,7 +67,7 @@ rules:
   - update
   - watch
 - apiGroups:
-  - source.fluxcd.io
+  - source.toolkit.fluxcd.io
   resources:
   - helmrepositories/status
   verbs:

PROJECT
@@ -1,4 +1,4 @@
-domain: fluxcd.io
+domain: toolkit.fluxcd.io
 repo: github.com/fluxcd/source-controller
 resources:
 - group: source

api/v1alpha1/doc.go
@@ -16,5 +16,5 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the source v1alpha1 API group
 // +kubebuilder:object:generate=true
-// +groupName=source.fluxcd.io
+// +groupName=source.toolkit.fluxcd.io
 package v1alpha1

api/v1alpha1/groupversion_info.go
@@ -23,7 +23,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "source.fluxcd.io", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "source.toolkit.fluxcd.io", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}

config/crd/bases/source.toolkit.fluxcd.io_gitrepositories.yaml
@@ -6,14 +6,14 @@ metadata:
   annotations:
     controller-gen.kubebuilder.io/version: v0.3.0
   creationTimestamp: null
-  name: helmrepositories.source.fluxcd.io
+  name: gitrepositories.source.toolkit.fluxcd.io
 spec:
-  group: source.fluxcd.io
+  group: source.toolkit.fluxcd.io
   names:
-    kind: HelmRepository
-    listKind: HelmRepositoryList
-    plural: helmrepositories
-    singular: helmrepository
+    kind: GitRepository
+    listKind: GitRepositoryList
+    plural: gitrepositories
+    singular: gitrepository
   scope: Namespaced
   versions:
   - additionalPrinterColumns:
@@ -32,7 +32,7 @@ spec:
     name: v1alpha1
     schema:
       openAPIV3Schema:
-        description: HelmRepository is the Schema for the helmrepositories API
+        description: GitRepository is the Schema for the gitrepositories API
         properties:
           apiVersion:
             description: 'APIVersion defines the versioned schema of this representation
@@ -47,32 +47,83 @@ spec:
           metadata:
             type: object
           spec:
-            description: HelmRepositorySpec defines the reference to a Helm repository.
+            description: GitRepositorySpec defines the desired state of a Git repository.
             properties:
-              interval:
-                description: The interval at which to check the upstream for updates.
+              ignore:
+                description: Ignore overrides the set of excluded patterns in the
+                  .sourceignore format (which is the same as .gitignore). If not provided,
+                  a default will be used, consult the documentation for your version
+                  to find out what those are.
                 type: string
+              interval:
+                description: The interval at which to check for repository updates.
+                type: string
+              ref:
+                description: The git reference to checkout and monitor for changes,
+                  defaults to master branch.
+                properties:
+                  branch:
+                    description: The git branch to checkout, defaults to master.
+                    type: string
+                  commit:
+                    description: The git commit sha to checkout, if specified tag
+                      filters will be ignored.
+                    type: string
+                  semver:
+                    description: The git tag semver expression, takes precedence over
+                      tag.
+                    type: string
+                  tag:
+                    description: The git tag to checkout, takes precedence over branch.
+                    type: string
+                type: object
               secretRef:
-                description: The name of the secret containing authentication credentials
-                  for the Helm repository. For HTTP/S basic auth the secret must contain
-                  username and password fields. For TLS the secret must contain caFile,
-                  keyFile and caCert fields.
+                description: The secret name containing the Git credentials. For HTTPS
+                  repositories the secret must contain username and password fields.
+                  For SSH repositories the secret must contain identity, identity.pub
+                  and known_hosts fields.
                 properties:
                   name:
                     description: 'Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
                       TODO: Add other useful fields. apiVersion, kind, uid?'
                     type: string
                 type: object
-              url:
-                description: The Helm repository URL, a valid URL contains at least
-                  a protocol and host.
+              timeout:
+                description: The timeout for remote git operations like cloning, default
+                  to 20s.
                 type: string
+              url:
+                description: The repository URL, can be a HTTP or SSH address.
+                pattern: ^(http|https|ssh)://
+                type: string
+              verify:
+                description: Verify OpenPGP signature for the commit that HEAD points
+                  to.
+                properties:
+                  mode:
+                    description: Mode describes what git object should be verified,
+                      currently ('head').
+                    enum:
+                    - head
+                    type: string
+                  secretRef:
+                    description: The secret name containing the public keys of all
+                      trusted git authors.
+                    properties:
+                      name:
+                        description: 'Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
+                          TODO: Add other useful fields. apiVersion, kind, uid?'
+                        type: string
+                    type: object
+                required:
+                - mode
+                type: object
             required:
             - interval
             - url
             type: object
           status:
-            description: HelmRepositoryStatus defines the observed state of the HelmRepository.
+            description: GitRepositoryStatus defines the observed state of a Git repository.
             properties:
               artifact:
                 description: Artifact represents the output of the last successful
@@ -129,7 +180,8 @@ spec:
                   type: object
                 type: array
               url:
-                description: URL is the download link for the last index fetched.
+                description: URL is the download link for the artifact output of the
+                  last repository sync.
                 type: string
             type: object
         type: object

config/crd/bases/source.toolkit.fluxcd.io_helmcharts.yaml
@@ -6,19 +6,25 @@ metadata:
   annotations:
     controller-gen.kubebuilder.io/version: v0.3.0
   creationTimestamp: null
-  name: gitrepositories.source.fluxcd.io
+  name: helmcharts.source.toolkit.fluxcd.io
 spec:
-  group: source.fluxcd.io
+  group: source.toolkit.fluxcd.io
   names:
-    kind: GitRepository
-    listKind: GitRepositoryList
-    plural: gitrepositories
-    singular: gitrepository
+    kind: HelmChart
+    listKind: HelmChartList
+    plural: helmcharts
+    singular: helmchart
   scope: Namespaced
   versions:
   - additionalPrinterColumns:
-    - jsonPath: .spec.url
-      name: URL
+    - jsonPath: .spec.name
+      name: Name
+      type: string
+    - jsonPath: .spec.version
+      name: Version
+      type: string
+    - jsonPath: .spec.helmRepositoryRef.name
+      name: Repository
       type: string
     - jsonPath: .status.conditions[?(@.type=="Ready")].status
       name: Ready
@@ -32,7 +38,7 @@ spec:
     name: v1alpha1
     schema:
       openAPIV3Schema:
-        description: GitRepository is the Schema for the gitrepositories API
+        description: HelmChart is the Schema for the helmcharts API
         properties:
           apiVersion:
             description: 'APIVersion defines the versioned schema of this representation
@@ -47,87 +53,40 @@ spec:
           metadata:
             type: object
           spec:
-            description: GitRepositorySpec defines the desired state of a Git repository.
+            description: HelmChartSpec defines the desired state of a Helm chart.
             properties:
-              ignore:
-                description: Ignore overrides the set of excluded patterns in the
-                  .sourceignore format (which is the same as .gitignore). If not provided,
-                  a default will be used, consult the documentation for your version
-                  to find out what those are.
-                type: string
-              interval:
-                description: The interval at which to check for repository updates.
-                type: string
-              ref:
-                description: The git reference to checkout and monitor for changes,
-                  defaults to master branch.
-                properties:
-                  branch:
-                    description: The git branch to checkout, defaults to master.
-                    type: string
-                  commit:
-                    description: The git commit sha to checkout, if specified tag
-                      filters will be ignored.
-                    type: string
-                  semver:
-                    description: The git tag semver expression, takes precedence over
-                      tag.
-                    type: string
-                  tag:
-                    description: The git tag to checkout, takes precedence over branch.
-                    type: string
-                type: object
-              secretRef:
-                description: The secret name containing the Git credentials. For HTTPS
-                  repositories the secret must contain username and password fields.
-                  For SSH repositories the secret must contain identity, identity.pub
-                  and known_hosts fields.
+              helmRepositoryRef:
+                description: The name of the HelmRepository the chart is available
+                  at.
                 properties:
                   name:
                     description: 'Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
                       TODO: Add other useful fields. apiVersion, kind, uid?'
                     type: string
                 type: object
-              timeout:
-                description: The timeout for remote git operations like cloning, default
-                  to 20s.
+              interval:
+                description: The interval at which to check the Helm repository for
+                  updates.
                 type: string
-              url:
-                description: The repository URL, can be a HTTP or SSH address.
-                pattern: ^(http|https|ssh)://
+              name:
+                description: The name of the Helm chart, as made available by the
+                  referenced Helm repository.
                 type: string
-              verify:
-                description: Verify OpenPGP signature for the commit that HEAD points
-                  to.
-                properties:
-                  mode:
-                    description: Mode describes what git object should be verified,
-                      currently ('head').
-                    enum:
-                    - head
-                    type: string
-                  secretRef:
-                    description: The secret name containing the public keys of all
-                      trusted git authors.
-                    properties:
-                      name:
-                        description: 'Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
-                          TODO: Add other useful fields. apiVersion, kind, uid?'
-                        type: string
-                    type: object
-                required:
-                - mode
-                type: object
+              version:
+                description: The chart version semver expression, defaults to latest
+                  when omitted.
+                type: string
             required:
+            - helmRepositoryRef
             - interval
-            - url
+            - name
             type: object
           status:
-            description: GitRepositoryStatus defines the observed state of a Git repository.
+            description: HelmChartStatus defines the observed state of the HelmChart.
             properties:
               artifact:
                 description: Artifact represents the output of the last successful
-                  repository sync.
+                  chart sync.
                 properties:
                   lastUpdateTime:
                     description: LastUpdateTime is the timestamp corresponding to
@@ -180,8 +139,7 @@ spec:
                   type: object
                 type: array
               url:
-                description: URL is the download link for the artifact output of the
-                  last repository sync.
+                description: URL is the download link for the last chart pulled.
                 type: string
             type: object
         type: object

config/crd/bases/source.toolkit.fluxcd.io_helmrepositories.yaml
@@ -6,25 +6,19 @@ metadata:
   annotations:
     controller-gen.kubebuilder.io/version: v0.3.0
   creationTimestamp: null
-  name: helmcharts.source.fluxcd.io
+  name: helmrepositories.source.toolkit.fluxcd.io
 spec:
-  group: source.fluxcd.io
+  group: source.toolkit.fluxcd.io
   names:
-    kind: HelmChart
-    listKind: HelmChartList
-    plural: helmcharts
-    singular: helmchart
+    kind: HelmRepository
+    listKind: HelmRepositoryList
+    plural: helmrepositories
+    singular: helmrepository
   scope: Namespaced
   versions:
   - additionalPrinterColumns:
-    - jsonPath: .spec.name
-      name: Name
-      type: string
-    - jsonPath: .spec.version
-      name: Version
-      type: string
-    - jsonPath: .spec.helmRepositoryRef.name
-      name: Repository
+    - jsonPath: .spec.url
+      name: URL
       type: string
     - jsonPath: .status.conditions[?(@.type=="Ready")].status
       name: Ready
@@ -38,7 +32,7 @@ spec:
     name: v1alpha1
     schema:
       openAPIV3Schema:
-        description: HelmChart is the Schema for the helmcharts API
+        description: HelmRepository is the Schema for the helmrepositories API
         properties:
           apiVersion:
             description: 'APIVersion defines the versioned schema of this representation
@@ -53,40 +47,36 @@ spec:
           metadata:
             type: object
           spec:
-            description: HelmChartSpec defines the desired state of a Helm chart.
+            description: HelmRepositorySpec defines the reference to a Helm repository.
             properties:
-              helmRepositoryRef:
-                description: The name of the HelmRepository the chart is available
-                  at.
+              interval:
+                description: The interval at which to check the upstream for updates.
+                type: string
+              secretRef:
+                description: The name of the secret containing authentication credentials
+                  for the Helm repository. For HTTP/S basic auth the secret must contain
+                  username and password fields. For TLS the secret must contain caFile,
+                  keyFile and caCert fields.
                 properties:
                   name:
                     description: 'Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names
                       TODO: Add other useful fields. apiVersion, kind, uid?'
                     type: string
                 type: object
-              interval:
-                description: The interval at which to check the Helm repository for
-                  updates.
-                type: string
-              name:
-                description: The name of the Helm chart, as made available by the
-                  referenced Helm repository.
-                type: string
-              version:
-                description: The chart version semver expression, defaults to latest
-                  when omitted.
+              url:
+                description: The Helm repository URL, a valid URL contains at least
+                  a protocol and host.
                 type: string
             required:
-            - helmRepositoryRef
             - interval
-            - name
+            - url
             type: object
           status:
-            description: HelmChartStatus defines the observed state of the HelmChart.
+            description: HelmRepositoryStatus defines the observed state of the HelmRepository.
             properties:
               artifact:
                 description: Artifact represents the output of the last successful
-                  chart sync.
+                  repository sync.
                 properties:
                   lastUpdateTime:
                     description: LastUpdateTime is the timestamp corresponding to
@@ -139,7 +129,7 @@ spec:
                   type: object
                 type: array
               url:
-                description: URL is the download link for the last chart pulled.
+                description: URL is the download link for the last index fetched.
                 type: string
             type: object
         type: object

config/crd/kustomization.yaml
@@ -1,8 +1,7 @@
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
-- bases/source.fluxcd.io_gitrepositories.yaml
-- bases/source.fluxcd.io_helmrepositories.yaml
-- bases/source.fluxcd.io_helmcharts.yaml
+- bases/source.toolkit.fluxcd.io_gitrepositories.yaml
+- bases/source.toolkit.fluxcd.io_helmrepositories.yaml
+- bases/source.toolkit.fluxcd.io_helmcharts.yaml
 # +kubebuilder:scaffold:crdkustomizeresource
-

fluxcd/source-watcher

commit 5bbb5abf7338cf3f32c027cbd1512198d5c0345f

fix(kubebuilder): correct group path for resources
This bug was originally caused by https://github.com/fluxcd/source-controller/commit/8e1b213da5d31527432cced4fe9f93167fe0d535

This fix allows kubebuilder to create the correct RBAC

Signed-off-by: David Beck <david.beck@invitae.com>

config/rbac/role.yaml
@@ -7,7 +7,7 @@ metadata:
   name: source-reader
 rules:
 - apiGroups:
-  - source.fluxcd.io
+  - source.toolkit.fluxcd.io
   resources:
   - gitrepositories
   verbs:
@@ -15,7 +15,7 @@ rules:
   - list
   - watch
 - apiGroups:
-  - source.fluxcd.io
+  - source.toolkit.fluxcd.io
   resources:
   - gitrepositories/status
   verbs:

controllers/gitrepository_watcher.go
@@ -39,8 +39,8 @@ type GitRepositoryWatcher struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=source.fluxcd.io,resources=gitrepositories,verbs=get;list;watch
-// +kubebuilder:rbac:groups=source.fluxcd.io,resources=gitrepositories/status,verbs=get
+// +kubebuilder:rbac:groups=source.toolkit.fluxcd.io,resources=gitrepositories,verbs=get;list;watch
+// +kubebuilder:rbac:groups=source.toolkit.fluxcd.io,resources=gitrepositories/status,verbs=get
 
 func (r *GitRepositoryWatcher) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := logr.FromContext(ctx)